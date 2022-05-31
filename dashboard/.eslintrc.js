79e20a5327052f18daf381ee9d1f34a625c47e97
Leandro Cavalcante <leandroguitarjesus89@gmail.com>
2022-05-31 10:57:07 -0300

    chore: added infos

--- dashboard/.eslintrc.js
+++ dashboard/.eslintrc.js
@@ -1,4 +1,4 @@
-module.exports = {
+odule.exports = {
   root: true,
   env: {
     node: true
@@ -8,16 +8,18 @@ module.exports = {
     '@vue/standard'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 0,
   },
   overrides: [
     {
       files: [
-        '**/*.spec.js'
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
